06279b77a17a9a1ecf01ca8f0f1db8f554c97b13
Paul Swingle <swingle8paul@gmail.com>
2025-11-05 18:42:53 -0800

    segment tree

---
+++ algorithms/segmenttree.py
@@ -0,0 +1,28 @@
+tree = [0] * (2 * n)
+for i in range(n):
+    tree[n + i] = arr[i]
+for i in range(n - 1, 0, -1):
+    tree[i] = tree[i << 1] + tree[i << 1 | 1] # change op
+
+def update(p, value): 
+    p = p + n
+    tree[p] = value; 
+    i = p   
+    while i > 1:
+        tree[i >> 1] = tree[i] + tree[i ^ 1] # change op
+        i >>= 1
+
+def query(l, r):
+    res = 0
+    l += n
+    r += n
+    while l < r:
+        if l & 1:
+            res += tree[l] # change op
+            l += 1
+        if r & 1:
+            r -= 1
+            res += tree[r] # change op
+        l >>= 1
+        r >>= 1
+    return res
